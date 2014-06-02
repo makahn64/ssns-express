13d2b1ab2fb2d091df6c8308a795f648b73c4bc1
Mitch Kahn <mitch@appdelegates.com>
2014-06-02 14:06:55 -0700

    index.js getting some settings work,

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var settings = require("../settings_module.js");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'ShareStation Server' });
+  var s = settings.getSettings();
+  res.render('index', { title: 'ShareStation Server', eventId: s.eventId, eventName: s.eventName });
 });
 
+router.post('/settings', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+            var s = settings.getSettings();
+            s.eventId = fields.eventId;
+            settings.setSettings(s);
+            res.writeHead(200, {'content-type': 'text/plain'});
+            res.write('received upload:\n\n');
+            res.end(util.inspect({fields: fields, files: files}));
+        });
+    });
+
+});
 
 router.get('/mktest', function(req, res) {
     res.render('mktest', { title: 'MK Test', author: 'Some Dude' });
